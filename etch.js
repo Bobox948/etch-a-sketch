b19f8e8a0a6a68c40d264defd7eb00328f058eb0
Boris <boris.pietrzak@gmail.com>
2022-11-01 16:21:51 +0100

    final version with rainbow colors

--- etch.js
+++ etch.js
@@ -4,6 +4,7 @@
 const container = document.getElementById('container1');
 
 var userinput=16;
+var colors = ["blue", "green", "red", "black", "purple", "yellow", "orange", "brown", "pink", "gray", ]
 
 
 
@@ -25,13 +26,81 @@ for (let i=0; i<userinput; i++){
         
         
     }
+
+    var divcolor = document.getElementsByClassName("content");
+
+var divcolor2 = Array.from(divcolor)
+
+
+for (let i=0;i < divcolor2.length;i++){
+
+divcolor2[i].addEventListener('mouseover', function(){
+
+
+var random = Math.floor((Math.random() * colors.length));
+var random2 = colors[random]
+    
+
+
+    divcolor2[i].style.backgroundColor= random2;
+
+
+    
+})
+}
+
     
    
 }
 
+let gridselect = document.getElementById('grid')
+
+gridselect.addEventListener('click', selection)
+
+function selection(){
+
+let element = document.getElementsByClassName("content");
+
+
+let element2= Array.from(element)
+
+
+for (let i=0; i<element2.length; i++){
+
+    element2[i].remove();
+}
 
 
 
+ let ask = prompt('Size of grid?')
+ userinput = Number(ask)
+if (userinput >100 || userinput=="" || userinput==null || isNaN(userinput)){ 
+    alert("Maximum grid 64x64, an enter a number");
+    userinput = 16;
+
+
+for (let i=0; i<userinput; i++){
+
+    var div1 = document.createElement('div');
+    div1.classList.add('break');
+    container.appendChild(div1);
+
+    
+   
+    for (let j=0; j<userinput; j++)
+    {
+        var div1 = document.createElement('div');
+        div1.classList.add('content');
+        container.appendChild(div1);
+        
+        
+        
+        
+    }
+    
+   
+}
+ 
 var divcolor = document.getElementsByClassName("content");
 
 var divcolor2 = Array.from(divcolor)
@@ -39,11 +108,63 @@ var divcolor2 = Array.from(divcolor)
 
 for (let i=0;i < divcolor2.length;i++){
 
-divcolor2[i].addEventListener("mouseover", function(){
+divcolor2[i].addEventListener('mouseover', function(){
 
-    divcolor2[i].style.backgroundColor= "red";
 
+    var random = Math.floor((Math.random() * colors.length));
+    var random2 = colors[random]
+        
+    
+    
+        divcolor2[i].style.backgroundColor= random2;
+    
+})
+} }
+else{
+ for (let i=0; i<userinput; i++){
+
+    var div1 = document.createElement('div');
+    div1.classList.add('break');
+    container.appendChild(div1);
+
+    
+   
+    for (let j=0; j<userinput; j++)
+    {
+        var div1 = document.createElement('div');
+        div1.classList.add('content');
+        container.appendChild(div1);
+        
+        
+        
+        
+    }
+    
+   
+}
+ 
+var divcolor = document.getElementsByClassName("content");
+
+var divcolor2 = Array.from(divcolor)
+
+
+for (let i=0;i < divcolor2.length;i++){
+
+divcolor2[i].addEventListener('mouseover', function(){
+
+    var random = Math.floor((Math.random() * colors.length));
+    var random2 = colors[random]
+        
+    
+    
+        divcolor2[i].style.backgroundColor= random2;
     
 })
 }
+}
+}
+
+
+
+
 
